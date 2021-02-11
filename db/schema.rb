689bf4fcf4f7dc49cd0299da1575b42a22fcd831
sasagawa-kota <sasagawa.kota0924@gmail.com>
2021-02-11 04:39:53 +0000

    カテゴリーテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_042816) do
+ActiveRecord::Schema.define(version: 2021_02_11_043635) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "topic_id"

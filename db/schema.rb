4000932cc49b81326da2f9bb3cb859bd187aee1c
sasagawa-kota <sasagawa.kota0924@gmail.com>
2021-02-13 11:35:30 +0000

    アドミンフラグ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_153423) do
+ActiveRecord::Schema.define(version: 2021_02_13_113236) do
 
   create_table "bookmarks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_02_12_153423) do
     t.integer "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

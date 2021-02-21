1d859668bfdd54a35879dd73bb216078f3a120a1
sasagawa-kota <sasagawa.kota0924@gmail.com>
2021-02-21 03:16:39 +0000

    ナビゲーションバー（仮）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   
+  protect_from_forgery with: :exception
+  add_flash_types :success, :info, :warning, :denger
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+  
   private
   
   def current_user
